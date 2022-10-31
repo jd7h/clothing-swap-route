86712c3f265347d1c2b9a0324b0097bd482ef78f
Judith van Stegeren <690008+jd7h@users.noreply.github.com>
2022-10-31 20:33:14 +0100

    Add precommit and lint all files

--- clothing_loop/data_io.py
+++ clothing_loop/data_io.py
@@ -1,10 +1,10 @@
 import csv
 import sys
-
 from os import path
 
+
 def read_participants(filename):
-    with open(filename, newline='') as csvfile:
+    with open(filename, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         participants = [row for row in reader]
 
--- clothing_loop/main.py
+++ clothing_loop/main.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python
 
-import sys
 import pprint
-
+import sys
 from argparse import ArgumentParser
+
 from data_io import read_participants
+
 
 def init_argument_parser():
     argparser = ArgumentParser()
     # -h/--help is automatically provided by ArgumentParser
-    argparser.add_argument("-d", "--debug", dest="debug", action="store_true",
-                         help="Output intermediate debugging lines to standard error, default: %(default)s",
-                         default=False)
-    argparser.add_argument("-i", "--input", dest="infile",
-                         help="Read the CSV input from this filename, or - for standard input, default: %(default)s")
-    argparser.add_argument("-o", "--output", dest="outfile",
-                         help="Store the output CSV in this filename, or - for standard output, default: %(default)s",
-                         default="-")
+    argparser.add_argument(
+        "-d",
+        "--debug",
+        dest="debug",
+        action="store_true",
+        help="Output intermediate debugging lines to standard error, default: %(default)s",
+        default=False,
+    )
+    argparser.add_argument(
+        "-i",
+        "--input",
+        dest="infile",
+        help="Read the CSV input from this filename, or - for standard input, default: %(default)s",
+    )
+    argparser.add_argument(
+        "-o",
+        "--output",
+        dest="outfile",
+        help="Store the output CSV in this filename, or - for standard output, default: %(default)s",
+        default="-",
+    )
     return argparser
 
+
 def main():
-    argparser = init_argument_parser();
-    args = argparser.parse_args();
+    argparser = init_argument_parser()
+    args = argparser.parse_args()
 
     if args.infile is None or args.outfile is None:
         argparser.print_help()
@@ -31,6 +46,7 @@ def main():
     if args.debug:
         print(f"[DBG] read_participants({args.infile}):", file=sys.stderr)
         pprint.pprint(participants, stream=sys.stderr)
+
 
 if __name__ == "__main__":
     main()
