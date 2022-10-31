4a88475da1f0c98b6aa553a2c5ea216857880486
Judith van Stegeren <690008+jd7h@users.noreply.github.com>
2022-10-31 20:42:09 +0100

    Create Hamilton cycles for participants (#15)

---
+++ clothing_loop/data_io.py
@@ -0,0 +1,11 @@
+import csv
+import sys
+
+from os import path
+
+def read_participants(filename):
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        participants = [row for row in reader]
+
+    return participants
